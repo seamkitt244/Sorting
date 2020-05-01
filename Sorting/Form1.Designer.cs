8481343451aed15ac0cfe3de4391f04aa883fd9c
Tony Theodoropoulos <theodot@outlook.com>
2020-05-01 10:54:24 -0400

    Add project files.

---
+++ Sorting/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalOutput = new System.Windows.Forms.Label();
+            this.sortedOutput = new System.Windows.Forms.Label();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.sortedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // originalOutput
+            // 
+            this.originalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalOutput.Location = new System.Drawing.Point(28, 55);
+            this.originalOutput.Name = "originalOutput";
+            this.originalOutput.Size = new System.Drawing.Size(130, 250);
+            this.originalOutput.TabIndex = 1;
+            this.originalOutput.Text = "label1";
+            this.originalOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sortedOutput
+            // 
+            this.sortedOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortedOutput.Location = new System.Drawing.Point(215, 55);
+            this.sortedOutput.Name = "sortedOutput";
+            this.sortedOutput.Size = new System.Drawing.Size(121, 250);
+            this.sortedOutput.TabIndex = 2;
+            this.sortedOutput.Text = "label1";
+            this.sortedOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.AutoSize = true;
+            this.originalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalLabel.Location = new System.Drawing.Point(27, 30);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(131, 25);
+            this.originalLabel.TabIndex = 3;
+            this.originalLabel.Text = "Original Array";
+            // 
+            // sortedLabel
+            // 
+            this.sortedLabel.AutoSize = true;
+            this.sortedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortedLabel.Location = new System.Drawing.Point(214, 30);
+            this.sortedLabel.Name = "sortedLabel";
+            this.sortedLabel.Size = new System.Drawing.Size(122, 25);
+            this.sortedLabel.TabIndex = 4;
+            this.sortedLabel.Text = "Sorted Array";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 342);
+            this.Controls.Add(this.sortedLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.sortedOutput);
+            this.Controls.Add(this.originalOutput);
+            this.Name = "Form1";
+            this.Text = "Sorting Examples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label originalOutput;
+        private System.Windows.Forms.Label sortedOutput;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.Label sortedLabel;
+    }
+}
+
---
+++ Sorting/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        int[] originalArray = { 3, 6, 1, 8, 5, 2 };
+        int[] sortedArray = new int[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // The 0 parameter indicates what index value in the sorted array
+            // to start adding the sorted values to. 
+            originalArray.CopyTo(sortedArray, 0);
+
+            //uncomment whichever sort method you want to test
+            selection(sortedArray);
+            //bubble(sortedArray);
+            //insertion(sortedArray);
+
+            originalOutput.Text = sortedOutput.Text = "";
+
+            foreach (int i in originalArray)
+            {
+                originalOutput.Text += i + "\n";
+            }
+
+            foreach (int i in sortedArray)
+            {
+                sortedOutput.Text += i + "\n";
+            }
+        }
+
+        public void selection(int[] tempArray)
+        {
+            int temp;
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                for (int j = i + 1; j < tempArray.Length; j++)
+                {
+                    if (tempArray[j] < tempArray[i])
+                    {
+                        temp = tempArray[i];
+                        tempArray[i] = tempArray[j];
+                        tempArray[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        public void bubble(int[] tempArray)
+        {
+            int bottom = tempArray.Length-1;
+            int temp;
+            Boolean sw = true;
+
+            while (sw == true)
+            {
+                sw = false;
+
+                for (int j = 0; j < bottom; j++)
+                {
+                    if (tempArray[j] > tempArray[j + 1])
+                    {
+                        sw = true;
+                        temp = tempArray[j];
+                        tempArray[j] = tempArray[j + 1];
+                        tempArray[j + 1] = temp;
+                    }
+                }
+                bottom--;
+            }
+        }
+
+
+
+        public void insertion(int[] tempArray)
+        {
+            int temp, j;
+
+            for (int n = 0; n < tempArray.Length; n++)
+            {
+                temp = tempArray[n];
+                j = n - 1;
+                
+                    while (j >= 0 && tempArray[j] > temp)
+                    {
+                        tempArray[j + 1] = tempArray[j];
+                        j--;
+                    }
+                
+                tempArray[j + 1] = temp;
+            }
+        }
+    }
+}
